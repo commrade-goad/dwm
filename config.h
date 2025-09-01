5ce00572470e322a5d6313e42fada748509a6394
Fernando Perry <fernandoperry1234@gmail.com>
2025-09-02 00:00:45 +0700

    added xresource patch

--- config.h
+++ config.h
@@ -2,28 +2,28 @@
 #include "movestack.c"
 
 /* some settings */
-static const unsigned int borderpx       = 2;   /* border pixel of windows */
-static const unsigned int snap           = 32;  /* snap pixel */
+static unsigned int borderpx       = 2;   /* border pixel of windows */
+static unsigned int snap           = 32;  /* snap pixel */
 static const unsigned int gappx          = 4;        /* gaps between windows */
-static const int showbar                 = 1;   /* 0 means no bar */
-static const int topbar                  = 1;   /* 0 means bottom bar */
+static int showbar                 = 1;   /* 0 means no bar */
+static int topbar                  = 1;   /* 0 means bottom bar */
 static const int showsystray             = 1;   /* 0 means no systray */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const char *fonts[]               = { "Iosevka Nerd Font Mono:size=11" };
-static const char dmenufont[]            = "Iosevka Nerd Font Mono:size=11";
-static const char col_gray1[]            = "#2D353B";
-static const char col_gray2[]            = "#3D484D";
-static const char col_gray3[]            = "#7A8478";
-static const char col_gray4[]            = "#D3C6AA";
-static const char col_cyan[]             = "#A7C080";
-static const char *colors[][3]           =
-{
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+static const char *fonts[]               = { "IosevkaTerm Nerd Font Mono:size=11" };
+static const char dmenufont[]            = "IosevkaTerm Nerd Font Mono:size=11";
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* some commands */
@@ -32,13 +32,13 @@ static const char *down_vol[]     = { "pamixer-wrapper",       "lower",      NUL
 static const char *mute_vol[]     = { "pamixer-wrapper",       "toggle",     NULL };
 static const char *brighter[]     = { "brightnessctl-wrapper", "raise",      NULL };
 static const char *dimmer[]       = { "brightnessctl-wrapper", "lower",      NULL };
-static const char *emoji[]        = { "dmenumoji",                           NULL };
 static const char *toggle_mpris[] = {"playerctl",              "play-pause", NULL };
 static const char *next_mpris[]   = {"playerctl",              "next"      , NULL };
 static const char *prev_mpris[]   = {"playerctl",              "previous"  , NULL };
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *emoji[]    = { "dmenumoji", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 /* tagging */
@@ -58,9 +58,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact        = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster        = 1;    /* number of clients in master area */
-static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
+static float mfact        = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster        = 1;    /* number of clients in master area */
+static int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -81,51 +81,72 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-    { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
-	/*{ MODKEY,                       XK_Return, zoom,           {0} },*/
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  _setgapsfr,     {.i = gappx  } },
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+    { "normbgcolor",        STRING,  &normbgcolor },
+    { "normbordercolor",    STRING,  &normbordercolor },
+    { "normfgcolor",        STRING,  &normfgcolor },
+    { "selbgcolor",         STRING,  &selbgcolor },
+    { "selbordercolor",     STRING,  &selbordercolor },
+    { "selfgcolor",         STRING,  &selfgcolor },
+    { "borderpx",          	INTEGER, &borderpx },
+    { "snap",          		INTEGER, &snap },
+    { "showbar",          	INTEGER, &showbar },
+    { "topbar",          	INTEGER, &topbar },
+    { "nmaster",          	INTEGER, &nmaster },
+    { "resizehints",       	INTEGER, &resizehints },
+    /* { "mfact",      	 	FLOAT,   &mfact }, */
+};
 
-    { 0,                XF86XK_AudioMute,                 spawn,         {.v = mute_vol } },
-    { 0,                XF86XK_AudioLowerVolume,          spawn,         {.v = down_vol } },
-    { 0,                XF86XK_AudioRaiseVolume,          spawn,         {.v = up_vol } },
-    { 0,                XF86XK_MonBrightnessDown,         spawn,         {.v = dimmer } },
-    { 0,                XF86XK_MonBrightnessUp,           spawn,         {.v = brighter } },
-    { 0,                XF86XK_AudioPlay,                 spawn,         {.v = toggle_mpris} },
-    { 0,                XF86XK_AudioNext,                 spawn,         {.v = next_mpris} },
-    { 0,                XF86XK_AudioPrev,                 spawn,         {.v = prev_mpris} },
-    { MODKEY|ShiftMask, XK_f,                             togglefullscr, {0} },
-    { MODKEY,           XK_period,                        spawn,         {.v = emoji } },
-    { MODKEY|ShiftMask, XK_p,                             spawn,         {.v = toggle_mpris} },
-    { MODKEY,           XK_s,                             togglesticky,  {0} },
-    { 0,                XK_Print,                         spawn,         SHCMD("xsc.sh") },
-    { ShiftMask,        XK_Print,                         spawn,         SHCMD("xscsel.sh") },
+static const Key keys[] = {
+	/* modifier                     key        function         argument */
+	{ MODKEY,                       XK_p,      spawn,           {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,           {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,       {0} },
+	{ MODKEY,                       XK_j,      focusstack,      {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,      {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,      {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,      {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,        {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,        {.f = +0.05} },
+    { MODKEY|ShiftMask,             XK_j,      movestack,       {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_k,      movestack,       {.i = -1 } },
+	// { MODKEY|ShiftMask,             XK_r,      reload_xresource, NULL },
+	{ MODKEY,                       XK_Tab,    view,            {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,      {0} },
+	{ MODKEY,                       XK_t,      setlayout,       {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,       {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,       {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,       {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating,  {0} },
+	{ MODKEY,                       XK_0,      view,            {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,             {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,        {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,          {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,          {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,         {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  _setgapsfr,      {.i = gappx  } },
+
+    { 0,                  XF86XK_AudioMute,                 spawn,         {.v = mute_vol } },
+    { 0,                  XF86XK_AudioLowerVolume,          spawn,         {.v = down_vol } },
+    { 0,                  XF86XK_AudioRaiseVolume,          spawn,         {.v = up_vol } },
+    { 0,                  XF86XK_MonBrightnessDown,         spawn,         {.v = dimmer } },
+    { 0,                  XF86XK_MonBrightnessUp,           spawn,         {.v = brighter } },
+    { 0,                  XF86XK_AudioPlay,                 spawn,         {.v = toggle_mpris} },
+    { 0,                  XF86XK_AudioNext,                 spawn,         {.v = next_mpris} },
+    { 0,                  XF86XK_AudioPrev,                 spawn,         {.v = prev_mpris} },
+    { MODKEY|ShiftMask,   XK_f,                             togglefullscr, {0} },
+    { MODKEY,             XK_period,                        spawn,         {.v = emoji } },
+    { MODKEY|ShiftMask,   XK_p,                             spawn,         {.v = toggle_mpris} },
+    { MODKEY,             XK_s,                             togglesticky,  {0} },
+    { 0,                  XK_Print,                         spawn,         SHCMD("xsc.sh") },
+    { ShiftMask,          XK_Print,                         spawn,         SHCMD("xscsel.sh") },
+    { ControlMask|MODKEY, XK_space,                         spawn,         SHCMD("~/.local/share/bin/togglekb.sh") },
+
 
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
