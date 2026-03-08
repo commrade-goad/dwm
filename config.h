470707ce3041ff325c578d5488371a4e97210cae
Fernando Perry <fernandoperry1234@gmail.com>
2026-03-08 16:25:17 +0700

    small dwm update

--- config.h
+++ config.h
@@ -2,9 +2,11 @@
 #include "movestack.c"
 
 /* some settings */
+#define USED_FONT "ComicShannsMono Nerd Font Mono:size=11"
+
 static unsigned int borderpx             = 2;   /* border pixel of windows */
 static unsigned int snap                 = 32;  /* snap pixel */
-static const unsigned int gappx          = 0;        /* gaps between windows */
+static const unsigned int gappx          = 4;   /* gaps between windows */
 static int showbar                       = 1;   /* 0 means no bar */
 static int topbar                        = 1;   /* 0 means bottom bar */
 static const int showsystray             = 1;   /* 0 means no systray */
@@ -12,8 +14,8 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const char *fonts[]               = { "monospace:size=10" };
-static const char dmenufont[]            = "monospace:size=10";
+static const char *fonts[]               = { USED_FONT };
+static const char dmenufont[]            = USED_FONT;
 static const int refreshrate             = 120;  /* refresh rate (per second) for client move/resize */
 static char normbgcolor[]                = "#222222";
 static char normbordercolor[]            = "#444444";
@@ -42,7 +44,7 @@ static const char *prev_mpris[]   = {"playerctl",              "previous"  , NUL
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *emoji[]    = { "dmenumoji", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -58,6 +60,7 @@ static const Rule rules[] = {
     { "mpv"         ,  NULL,       NULL,       0,            1,           -1 },
     { "firefox"     ,  NULL,       NULL,       1<<1,         0,           -1 },
     { "vesktop"     ,  NULL,       NULL,       1<<2,         0,           -1 },
+    { "discord"     ,  NULL,       NULL,       1<<2,         0,           -1 },
     { "pavucontrol" ,  NULL,       NULL,       0,            1,           -1 },
 };
 
